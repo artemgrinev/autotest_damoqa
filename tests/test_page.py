1aa33d534734c17a46eb61a429a81246b7f87493
GrinevArtem <artemgrinyov90@gmail.com>
2022-10-20 22:06:11 +0300

    Create new class CheckBoxPage, CheckBoxPageLocators, TestCheckBox

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,4 +1,4 @@
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 import time
 
 
@@ -13,3 +13,15 @@ class TestElements:
             assert email == output_email, "The email does not match"
             assert current_address == output_cur_addr, "The current address does not match"
             assert permanent_address == output_perm_addr, "The permanent address does not match"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_check_box()
+            checked_checkbox = check_box_page.get_checked_checkbox()
+            output_result = check_box_page.get_output_result()
+            assert checked_checkbox == output_result, "checkboxes have not been selected"
+
+
